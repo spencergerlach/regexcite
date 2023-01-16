d2c47e26da88cabedae2ec77abd67a7570c6ccac
Spencer Gerlach <spence.gerlach@gmail.com>
2023-01-15 19:20:16 -0800

    fix typo in strsplit.R

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -8,7 +8,7 @@
 #'
 #' @examples
 #' x <- "alfa,bravo,charlie,delta"
-#' strplit1(x, split = ",")
+#' strsplit1(x, split = ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
